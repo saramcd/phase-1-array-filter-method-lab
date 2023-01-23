33b7d4da3f2676c5f2b02cbbfe9d96c3a6621331
Sara McDonnell <saramcdonnell32@gmail.com>
2023-01-23 15:42:07 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+const findMatching = (drivers, driverName) => {
+  const matchingDrivers = drivers.filter(function (name) {
+    return name.toLowerCase() === driverName.toLowerCase();
+  });
+  return matchingDrivers;
+};
+
+const fuzzyMatch = (drivers, driverName) => {
+  const fuzzyMatchingDrivers = drivers.filter(function (name) {
+    return name.toLowerCase().charAt(0) === driverName.toLowerCase().charAt(0);
+  });
+  return fuzzyMatchingDrivers;
+};
+
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+const matchName = (drivers, driverName) => {
+  const matchingDrivers = drivers.filter(function (name) {
+    return name.name === driverName;
+  });
+  return matchingDrivers;
+};
